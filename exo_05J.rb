b16fe27357078b8c441293cb42b8d8b2e7961697
JMD60260 <dardy.jeanmarc@gmail.com>
2019-07-09 12:26:22 +0200

    modif exo5 et 6

--- exo_05J.rb
+++ exo_05J.rb
@@ -1,4 +1,5 @@
 puts "On va compter le nombre d'heures de travail à THP"
+# #{} fait le calcul pour les deux exercices suivants
 puts "Travail : #{10 * 5 * 11}"
 puts "En minutes ça fait : #{10 * 5 * 11 * 60}"
 
@@ -9,14 +10,14 @@ puts 10 * 5 * 11 * 60 * 60
 puts "Est-ce que c'est vrai que 3 + 2 < 5 - 7 ?"
 
 puts 3 + 2 < 5 - 7
-
+# #{} fait le calcul suivant pour les deux prochaines lignes
 puts "Ça fait combien 3 + 2 ? #{3 + 2}"
 puts "Ça fait combien 5 - 7 ? #{5 - 7}"
 
 puts "Ok, c'est faux alors !"
 
 puts "C'est drôle ça, faisons-en plus :"
-
+# #{} demande booléenne pour les trois lignes suivantes
 puts "Est-ce que 5 est plus grand que -2 ? #{5 > -2}"
 puts "Est-ce que 5 est supérieur ou égal à -2 ? #{5 >= -2}"
 puts "Est-ce que 5 est inférieur ou égal à -2 ? #{5 <= -2}"
