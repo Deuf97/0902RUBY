4802b0008d0952b5c557e5a07c40f4ae3d2b9b51
DARDY Jean Marc <52466695+JMD60260@users.noreply.github.com>
2019-07-09 22:30:12 +0200

    Update exo_18J.rb

--- exo_18J.rb
+++ exo_18J.rb
@@ -5,9 +5,9 @@ mail = "@email.fr"
 email=Array.new
 1.step(50, 1) do |i|
     if i < 9
-     email<<prénom+nom+"0"+"#{i+1}"+mail
+     email << prénom+nom+"0"+"#{i+1}"+mail
     else
-      email<<prénom+nom+"#{i+1}"+mail
+      email << prénom+nom+"#{i+1}"+mail
     end
 end
 puts email
